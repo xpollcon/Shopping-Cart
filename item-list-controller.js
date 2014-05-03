27da2edd89ca0670026bc47c413c75e2a4b71d91
Borzou <borzoum@gmail.com>
2014-05-03 20:13:30 +1000

    file name fix

---
+++ item-list-controller.js
@@ -0,0 +1,8 @@
+var items = [{name:'item1',desc:'ABCD',photo_url:'sale.jpg'},
+            {name:'item2',desc:'ABCD',photo_url:'sale.jpg'},
+            {name:'item3',desc:'ABCD',photo_url:'sale.jpg'},
+            {name:'item4',desc:'ABCD',photo_url:'sale.jpg'}];
+
+function itemListController($scope){
+    $scope.items = items;
+}
---
+++ shopping-list-controller.js
@@ -0,0 +1,4 @@
+var items = [{name:'item1',desc:'ABCD',photo_url:'sale.jpg'},
+            {name:'item2',desc:'ABCD',photo_url:'sale.jpg'},
+            {name:'item3',desc:'ABCD',photo_url:'sale.jpg'},
+            {name:'item4',desc:'ABCD',photo_url:'sale.jpg'}];
